d5abce0987740ce154306c06e471db1629d1d706
Bruno Milare <milare@gmail.com>
2011-03-25 22:15:51 -0300

    demo

--- lib/redbis.rb
+++ lib/redbis.rb
@@ -2,45 +2,12 @@ require 'rubygems'
 require 'ruby-debug'
 require 'active_support'
 require 'active_support/all'
-require 'redbis/attributes'
-require 'redbis/connection'
-require 'redbis/callbacks'
-require 'redbis/validations'
-require 'redbis/errors'
-require 'redbis/base'
 require 'redis'
+require File.dirname(__FILE__) + '/redbis/attributes'
+require File.dirname(__FILE__) + '/redbis/connection'
+require File.dirname(__FILE__) + '/redbis/callbacks'
+require File.dirname(__FILE__) + '/redbis/validations'
+require File.dirname(__FILE__) + '/redbis/errors'
+require File.dirname(__FILE__) + '/redbis/base'
 
-class User < Redbis::Base
-
-  field :foo, :default => 1
-  field :bar, :default => 'foobar'
-  
-  use_key :people
-  
-end
-
-class Post < Redbis::Base
-
-  field :title, :default => 'post title'
-  field :body
-
-  before_initialize :test_before_callback
-  after_initialize :test_after_callback
-  before_validation :define_body_field
-
-  validates :presence, :body, :title
-
-  def test_before_callback
-    puts "before"
-  end
-
-  def test_after_callback
-    puts "after"
-  end
-
-  def define_body_field
-    self.body ||= "callback for body"
-  end
-
-end
 

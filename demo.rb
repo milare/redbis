bf7a329273b180d260e55ed2b2f6f408b85bf9ad
Bruno Milare <milare@gmail.com>
2011-03-25 22:15:59 -0300

    demo

---
+++ demo.rb
@@ -0,0 +1,48 @@
+require 'lib/redbis'
+
+class User < Redbis::Base
+
+  field :foo, :default => 1
+  field :bar, :default => 'foobar'
+  
+  use_key :people
+  
+end
+
+class Post < Redbis::Base
+
+  field :title, :default => 'post title'
+  field :body
+
+  before_initialize :test_before_callback
+  after_initialize :test_after_callback
+  before_validation :define_body_field
+
+  validates :presence, :body, :title
+
+  def test_before_callback
+    puts "before"
+  end
+
+  def test_after_callback
+    puts "after"
+  end
+
+  def define_body_field
+    self.body ||= "callback for body"
+  end
+end
+
+
+p = Post.new
+p.save
+
+p2 = Post.new(:title => 'new one')
+p2.save
+
+posts = Post.all
+p3 = Post.find_by_title p2.title
+
+p2 == p3
+
+
